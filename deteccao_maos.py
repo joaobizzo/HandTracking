987baf5e6a5580daaa008d9502991e07d748ca4e
João Bizzo <92961230+joaobizzo@users.noreply.github.com>
2023-06-13 15:44:25 -0300

    Update deteccao_maos.py

--- deteccao_maos.py
+++ deteccao_maos.py
@@ -1,4 +1,3 @@
-#importando bibliotecas
 import cv2
 import mediapipe as mp
 
@@ -7,30 +6,30 @@ num_hands = 2
 min_detection_confidence = 0.8
 min_tracking_confidence = 0.5
 #parametros desenho
-cor_linha = (224, 208, 64)
-cor_bola = (255, 255, 255)
+line_color = (224, 208, 64)
+ball_color = (255, 255, 255)
 
 
 
-mp_maos = mp.solutions.hands
-mp_desenho = mp.solutions.drawing_utils
-maos = mp_maos.Hands(num_hands = num_hands, min_detection_confidence=min_detection_confidence, min_tracking_confidence=min_tracking_confidence)
+mp_hands = mp.solutions.hands
+mp_draws = mp.solutions.drawing_utils
+maos = mp_hands.Hands(min_detection_confidence=min_detection_confidence, min_tracking_confidence=min_tracking_confidence)
 
 
 
 #por padrao, 0 ou 1
 #usuarios mac, podem usar a camera do celular, basta testar as duas opcoes (0 e 1)
-camera = cv2.VideoCapture()
+cam = cv2.VideoCapture(0)
 
 #resolucao da imagem
-resolucao_x = 1280
-resolucao_y = 720
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, resolucao_x)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, resolucao_y)
+resolution_x = 1280
+resolution_y = 720
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, resolution_x)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution_y)
 
 
-
-def verificar_sucesso_camera(sucesso_camera):
+# funcao para verificar se a camera foi iniciada
+def verify_cam_sucess(sucesso_camera):
     if sucesso_camera == True:
         terminal = "Camera iniciada"
         print(terminal)
@@ -38,6 +37,7 @@ def verificar_sucesso_camera(sucesso_camera):
         terminal = "Erro ao iniciar camera"
         print(terminal)
 
+# funcao para informar se a mao foi detectada corretamente
 def terminal_sucesso(sucesso):
     if sucesso == True:
         terminal = "Mao detectada"
@@ -46,6 +46,9 @@ def terminal_sucesso(sucesso):
         terminal = "Mao nao detectada"
         print(terminal)
 
+
+
+# funcao principal encontrar as coordenadas das maos
 def encontra_coord_maos(img, espelho = False):
     if espelho:
         #espelhar imagem
@@ -60,40 +63,49 @@ def encontra_coord_maos(img, espelho = False):
             info_mao = {}
             coordenadas = []
             for marcacao in marcacoes_maos.landmark:
-                coord_x, coord_y, coord_z = int(marcacao.x * resolucao_x), int(marcacao.y * resolucao_y), int(marcacao.z * resolucao_x)
+                coord_x, coord_y, coord_z = int(marcacao.x * resolution_x), int(marcacao.y * resolution_y), int(marcacao.z * resolution_x)
                 coordenadas.append([coord_x, coord_y, coord_z])
                 #print(coord_x, coord_y, coord_z)
             info_mao["coordenadas"] = coordenadas
-            if espelho:
-                if lado_mao.classification[0].label == "Left":
-                    info_mao["lado"] = "Right"
-                else:
-                    info_mao["lado"] = "Left"
-            else:
-                lado_mao["lado"] = lado_mao.classification[0].label
-            
-            print(info_mao["lado"])
 
             todas_maos.append(info_mao)
-            mp_desenho.draw_landmarks(img, marcacoes_maos, mp_maos.HAND_CONNECTIONS,
-                                    mp_desenho.DrawingSpec(color=cor_bola, thickness=2, circle_radius=2),
-                                    mp_desenho.DrawingSpec(color=cor_linha, thickness=2, circle_radius=2))
+            mp_draws.draw_landmarks(img, marcacoes_maos, mp_hands.HAND_CONNECTIONS,
+                                    mp_draws.DrawingSpec(color=ball_color, thickness=2, circle_radius=2),
+                                    mp_draws.DrawingSpec(color=line_color, thickness=2, circle_radius=2))
         sucesso = True
     else:
         sucesso = False
     
-    return img, lado_mao, todas_maos, sucesso
+    return img, todas_maos, sucesso
+
+
+# funcao para definir se a mao esta aberta ou fechada
+def dedos_levantados(mao):
+    dedos = []
+    for ponta_dedo in 8, 12, 16, 20:
+        if mao['coordenadas'][ponta_dedo][1] < mao['coordenadas'][ponta_dedo - 2][1]:
+            dedos.append(True)
+        else:
+            dedos.append(False)
+    return dedos
+
+
 
 #loop principal do codigo
-while camera.isOpened():
+while cam.isOpened():
 
-    sucesso_camera, img = camera.read()
-    verificar_sucesso_camera(sucesso_camera)
-    img, lado_mao, todas_mao, sucesso = encontra_coord_maos(img, espelho=True)
+    sucesso_camera, img = cam.read()
+    #verify_cam_sucess(sucesso_camera)
+    img, todas_maos, sucesso = encontra_coord_maos(img, espelho=True)
+
+    if len(todas_maos) == 1:
+        info_dedos_mao1 = dedos_levantados(todas_maos[0])
+        print(info_dedos_mao1)
     
     cv2.imshow("Imagem", img)
-    terminal_sucesso(sucesso)
+    #terminal_sucesso(sucesso)
 
     tecla = cv2.waitKey(1)
-    if tecla == 27:
+    if tecla == 27: # tecla esc
         break
+    

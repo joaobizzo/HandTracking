2814ae4d273fa2a18b7cf3832bba371c3a1a6246
João Bizzo <92961230+joaobizzo@users.noreply.github.com>
2023-07-08 23:53:14 -0300

    Atualizar o iris_tracking.py

--- iris_tracking.py
+++ iris_tracking.py
@@ -1,9 +1,12 @@
 import cv2
 import mediapipe as mp
 import pyautogui
+
 cam = cv2.VideoCapture(0)
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 screen_w, screen_h = pyautogui.size()
+
+# main loop
 while True:
     _, frame = cam.read()
     frame = cv2.flip(frame, 1)

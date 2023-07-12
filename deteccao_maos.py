55f051a89c78ff8d7438e397d75df19d2447bc0f
João Bizzo <92961230+joaobizzo@users.noreply.github.com>
2023-07-12 18:15:40 -0300

    Update deteccao_maos.py

--- deteccao_maos.py
+++ deteccao_maos.py
@@ -1,21 +1,24 @@
 import cv2
 import mediapipe as mp
+import os
+import datetime
+
 
 #definindo parametros da deteccao
-num_hands = 2
 min_detection_confidence = 0.8
 min_tracking_confidence = 0.5
 #parametros desenho
 line_color = (224, 208, 64)
 ball_color = (255, 255, 255)
-
-
+purple = (255, 0, 255)
+black = (10, 10, 10)
 
 mp_hands = mp.solutions.hands
 mp_draws = mp.solutions.drawing_utils
 maos = mp_hands.Hands(min_detection_confidence=min_detection_confidence, min_tracking_confidence=min_tracking_confidence)
 
-
+# Initialize the previous verification time
+prev_verification_time = datetime.datetime.now()
 
 #por padrao, 0 ou 1
 #usuarios mac, podem usar a camera do celular, basta testar as duas opcoes (0 e 1)
@@ -27,32 +30,23 @@ resolution_y = 720
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, resolution_x)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution_y)
 
-
-# funcao para verificar se a camera foi iniciada
-def verify_cam_sucess(sucesso_camera):
-    if sucesso_camera == True:
-        terminal = "Camera iniciada"
-        print(terminal)
-    else:
-        terminal = "Erro ao iniciar camera"
-        print(terminal)
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 # funcao para informar se a mao foi detectada corretamente
 def terminal_sucesso(sucesso):
-    if sucesso == True:
+    if sucesso:
         terminal = "Mao detectada"
-        print(terminal)
     else:
         terminal = "Mao nao detectada"
-        print(terminal)
-
+    print(terminal)
 
 
 # funcao principal encontrar as coordenadas das maos
-def encontra_coord_maos(img, espelho = False):
+def encontra_coord_maos(img, espelho=False):
     if espelho:
         #espelhar imagem
-        img = cv2.flip(img, 1) 
+        img = cv2.flip(img, 1)
     #converter de RGB para BGR(padrao opencv)
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     resultado = maos.process(img_rgb)
@@ -65,7 +59,6 @@ def encontra_coord_maos(img, espelho = False):
             for marcacao in marcacoes_maos.landmark:
                 coord_x, coord_y, coord_z = int(marcacao.x * resolution_x), int(marcacao.y * resolution_y), int(marcacao.z * resolution_x)
                 coordenadas.append([coord_x, coord_y, coord_z])
-                #print(coord_x, coord_y, coord_z)
             info_mao["coordenadas"] = coordenadas
 
             todas_maos.append(info_mao)
@@ -90,22 +83,39 @@ def dedos_levantados(mao):
     return dedos
 
 
-
 #loop principal do codigo
 while cam.isOpened():
-
     sucesso_camera, img = cam.read()
-    #verify_cam_sucess(sucesso_camera)
     img, todas_maos, sucesso = encontra_coord_maos(img, espelho=True)
-
-    if len(todas_maos) == 1:
-        info_dedos_mao1 = dedos_levantados(todas_maos[0])
-        print(info_dedos_mao1)
     
-    cv2.imshow("Imagem", img)
-    #terminal_sucesso(sucesso)
+    if len(todas_maos) == 1:
+        thumb_tip = todas_maos[0]['coordenadas'][4]
+        index_finger_tip = todas_maos[0]['coordenadas'][8]
+        # Calculate the Euclidean distance between thumb and index finger tips
+        distance = ((thumb_tip[0] - index_finger_tip[0])**2 + (thumb_tip[1] - index_finger_tip[1])**2)**0.5
 
+        info_dedos_mao1 = dedos_levantados(todas_maos[0])
+
+        #calculate the center of the hand
+        hand_x_center = todas_maos[0]['coordenadas'][9][0]
+        hand_y_center = todas_maos[0]['coordenadas'][9][1]
+        
+        cv2.circle(img, (hand_x_center, hand_y_center), 10, purple, -1)
+        
+        
+    
+    clear()
+    # Calculate the response time
+    current_time = datetime.datetime.now()
+    response_time = (current_time - prev_verification_time).total_seconds()
+    prev_verification_time = current_time
+
+    print("Response time:", response_time)
+    cv2.imshow("Imagem", img)
+    
     tecla = cv2.waitKey(1)
     if tecla == 27: # tecla esc
         break
-    
+
+cam.release()
+cv2.destroyAllWindows()

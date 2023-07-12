14e75054d0aeb6e7b42fc8d42f63134168f68e65
João Bizzo <92961230+joaobizzo@users.noreply.github.com>
2023-07-12 18:22:17 -0300

    Update hand_detection.py

--- hand_detection.py
+++ hand_detection.py
@@ -1,145 +1,122 @@
 import cv2
 import mediapipe as mp
 import os
-import webbrowser
-import time
+import datetime
 
-#  Setting detection parameters
-num_hands = 2
+
+#defining detection parameters
 min_detection_confidence = 0.8
 min_tracking_confidence = 0.5
-
-#  Drawing parameters
-
-# --colors in BGR: // TODO: create a function to put rgb and convert to bgr
+#drawing parameters
 line_color = (224, 208, 64)
 ball_color = (255, 255, 255)
-keyborad_color = (194, 178, 2)
-WHITE = (255, 255, 255)
-BLACK = (15, 15, 15)
-
+purple = (255, 0, 255)
+black = (10, 10, 10)
 
 mp_hands = mp.solutions.hands
 mp_draws = mp.solutions.drawing_utils
-hands = mp_hands.Hands(min_detection_confidence=min_detection_confidence, min_tracking_confidence=min_tracking_confidence)
+maos = mp_hands.Hands(min_detection_confidence=min_detection_confidence, min_tracking_confidence=min_tracking_confidence)
 
-#  By default, 0 or 1
-#  Mac users can use their phone's camera by testing both options (0 and 1)
+#initialize the previous verification time
+prev_verification_time = datetime.datetime.now()
+
+#by default, 0 or 1
+#Mac users can use their phone camera by testing both options (0 and 1)
 cam = cv2.VideoCapture(0)
 
-#  Image resolution
+#image resolution
 resolution_x = 1280
 resolution_y = 720
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, resolution_x)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution_y)
 
-# special parameters
-rock_and_roll = False
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-keys = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
-        ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-        ['Z', 'X', 'C', 'V','B', 'N', 'M', ',', '.', '']]
-offset = 80
-
-
-#  Function to verify if the camera was successfully initialized
-def verify_cam_success(camera_success):
-    if camera_success == True:
-        message = "Camera initialized"
-        print(message)
+# function to inform if the hand was detected correctly
+def terminal_sucesso(sucesso):
+    if sucesso:
+        terminal = "Mao detectada"
     else:
-        message = "Error initializing camera"
-    print(message)
+        terminal = "Mao nao detectada"
+    print(terminal)
 
-#  Function to inform whether the hand was correctly detected
-def success_terminal(success):
-    if success == True:
-        message = "Hand detected"
-    else:
-        message = "Hand not detected"
-    print(message)
 
-#  Main function to find hand coordinates
-def find_hand_coordinates(img, mirror=False):
-    if mirror:
-        #  Mirror the image
+# main function to find hand coordinates
+def encontra_coord_maos(img, espelho=False):
+    if espelho:
+        # mirror the image
         img = cv2.flip(img, 1)
-    #  Convert from RGB to BGR (OpenCV default)
+    # Convert from RGB to BGR (OpenCV default)
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    result = hands.process(img_rgb)
-    all_hands = []
-    if result.multi_hand_landmarks:
-        for hand_side, hand_landmarks in zip(result.multi_handedness, result.multi_hand_landmarks):
-            hand_info = {}
-            coordinates = []
-            for landmark in hand_landmarks.landmark:
-                coord_x, coord_y, coord_z = int(landmark.x * resolution_x), int(landmark.y * resolution_y), int(landmark.z * resolution_x)
-                coordinates.append([coord_x, coord_y, coord_z])
-                # print(coord_x, coord_y, coord_z)
-            hand_info["coordinates"] = coordinates
-            hand_info["side"] = hand_side.classification[0].label
+    resultado = maos.process(img_rgb)
+    todas_maos = []
 
-            all_hands.append(hand_info)
-            mp_draws.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+    if resultado.multi_hand_landmarks:
+        for lado_mao, marcacoes_maos in zip(resultado.multi_handedness, resultado.multi_hand_landmarks):
+            info_mao = {}
+            coordenadas = []
+            for marcacao in marcacoes_maos.landmark:
+                coord_x, coord_y, coord_z = int(marcacao.x * resolution_x), int(marcacao.y * resolution_y), int(marcacao.z * resolution_x)
+                coordenadas.append([coord_x, coord_y, coord_z])
+            info_mao["coordenadas"] = coordenadas
+
+            todas_maos.append(info_mao)
+            mp_draws.draw_landmarks(img, marcacoes_maos, mp_hands.HAND_CONNECTIONS,
                                     mp_draws.DrawingSpec(color=ball_color, thickness=2, circle_radius=2),
                                     mp_draws.DrawingSpec(color=line_color, thickness=2, circle_radius=2))
-        success = True
+        sucesso = True
     else:
-        success = False
-
-    return img, all_hands, success
-
-#  Function to determine if the hand is open or closed
-def count_raised_fingers(hand):
-    fingers = []
-    for finger_tip in 8, 12, 16, 20:
-        if hand['coordinates'][finger_tip][1] < hand['coordinates'][finger_tip - 2][1]:
-          fingers.append(True)
-        else:
-            fingers.append(False)
-    return fingers
-
-
-def print_keyboard(img, position, letter, size=50, color=WHITE):
-    cv2.rectangle(img, position, (position[0]+size, position[1]+size), keyborad_color, cv2.FILLED)
-    cv2.rectangle(img, position, (position[0]+size, position[1]+size), BLACK, 1)
-    cv2.putText(img, letter, (position[0]+15, position[1]+30), cv2.FONT_HERSHEY_PLAIN, 2, WHITE, 3)
-    return img
-
-#  Main code loop
-while cam.isOpened():
-    camera_success, img = cam.read()
-    # verify_cam_success(camera_success)
-
-    img, all_hands, success = find_hand_coordinates(img, mirror=True)
-
-    cv2.rectangle(img, (50, 50), (100, 100), keyborad_color, 2)
-    cv2.putText(img, 'X', (65, 85), cv2.FONT_HERSHEY_PLAIN, 2, WHITE, 3)
-
-
-    if len(all_hands) == 1:
-        hand1_finger_info = count_raised_fingers(all_hands[0])
-
-        if all_hands[0]['side'] == 'Left':
-            for index_line, keyboard_line in enumerate(keys):
-                for index, key in enumerate(keyboard_line):
-                    img = print_keyboard(img, (offset+index*60, offset+index_line*60), key)
-        
-        if all_hands[0]['side'] == 'Right':
-            if hand1_finger_info == [True, False, False, True] and rock_and_roll == False:
-                rock_and_roll = True
-                print("Rock and roll baby")
-                #time.sleep(2)
-                #webbrowser.open("https://www.youtube.com/watch?v=pAgnJDJN4VA")
-            
-
-
-        
-    cv2.imshow("Image", img)
-    # success_terminal(success)
-
+        sucesso = False
     
+    return img, todas_maos, sucesso
 
-    key = cv2.waitKey(1)
-    if key == 27: #esc key
+
+# function to determine if the hand is open or closed
+def dedos_levantados(mao):
+    dedos = []
+    for ponta_dedo in 8, 12, 16, 20:
+        if mao['coordenadas'][ponta_dedo][1] < mao['coordenadas'][ponta_dedo - 2][1]:
+            dedos.append(True)
+        else:
+            dedos.append(False)
+    return dedos
+
+
+# main loop of the cod
+while cam.isOpened():
+    sucesso_camera, img = cam.read()
+    img, todas_maos, sucesso = encontra_coord_maos(img, espelho=True)
+    
+    if len(todas_maos) == 1:
+        thumb_tip = todas_maos[0]['coordenadas'][4]
+        index_finger_tip = todas_maos[0]['coordenadas'][8]
+        # Calculate the Euclidean distance between thumb and index finger tips
+        distance = ((thumb_tip[0] - index_finger_tip[0])**2 + (thumb_tip[1] - index_finger_tip[1])**2)**0.5
+
+        info_dedos_mao1 = dedos_levantados(todas_maos[0])
+
+        #calculate the center of the hand
+        hand_x_center = todas_maos[0]['coordenadas'][9][0]
+        hand_y_center = todas_maos[0]['coordenadas'][9][1]
+        
+        cv2.circle(img, (hand_x_center, hand_y_center), 10, purple, -1)
+        
+        
+    
+    clear()
+    # Calculate the response time
+    current_time = datetime.datetime.now()
+    response_time = (current_time - prev_verification_time).total_seconds()
+    prev_verification_time = current_time
+
+    print("Response time:", response_time)
+    cv2.imshow("Imagem", img)
+    
+    tecla = cv2.waitKey(1)
+    if tecla == 27: #  esc
         break
+
+cam.release()
+cv2.destroyAllWindows()
+
